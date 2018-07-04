6cf783dcbaefab77e31665e82c0194e7f239b25d
sm-azure <you@example.com>
2018-07-04 12:15:00 +0530

    updated to work with required buttons

--- kitt_control.py
+++ kitt_control.py
@@ -18,10 +18,23 @@ else:
 # Print device capabilities
 print (control_f710.capabilities(verbose=True))
 
+# Define keys of interest
+BTNS = {305: "STARTUP", 306: "SHUTDOWN", 308: "ZERO_SPEED"}
+ABS = {02: "LATERAL", 17: "SPEED_STEP"}
+
 for event in control_f710.read_loop():
     if event.type == evdev.ecodes.EV_KEY:
-        print (event.code, event.value)
+        if event.code in BTNS.keys() and event.value!= 0L:
+            print (event.code, event.value, BTNS[event.code])
+
     if event.type == evdev.ecodes.EV_ABS:
-       print (event)
+        if event.code in ABS.keys():
+            # For lateral control range is from 0 (full left) -255 (full right)
+            # For step speed control values are +1 or -1 (ignore 0 as it is button return)
+            #print(event.code, event.value, ABS[event.code])
+            if((event.code == 17 and event.value !=0) or event.code == 02):
+                #print ('---')
+                print(event.code, event.value, ABS[event.code])
+
     
     
